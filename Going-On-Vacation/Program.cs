a9f9a0b1695da2b506d345c71be42d0f6f0d131a
Abdullah Alsalem <abdullahsalem18@gmail.com>
2021-05-05 02:42:18 +0300

    Done

---
+++ Going-On-Vacation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Going_On_Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tours = { "England", "Spain", "Italy", "Portugal", "France" };
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            //your code goes here
+            try
+            {
+                Console.WriteLine(tours[choice].Trim());
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Wrong number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
+    }
+}
